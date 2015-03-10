bf54ee522dfcd1bdee0a0db1c4b3ad4f29bcaf0a
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-10 17:23:15 +0100

    basic CLI app

--- antroid.go
+++ antroid.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/bfontaine/antroid/api"
 	"os"
 	"strings"
+)
+
+// just for tests
+const (
+	username = "ww"
+	password = "a"
 )
 
 func exitErr(e error) {
@@ -13,34 +20,48 @@ func exitErr(e error) {
 }
 
 func main() {
-	// just a demo
+	apiMethods := flag.Bool("api-methods", false, "show all API methods")
+	whoAmI := flag.Bool("whoami", false, "verify that the user is logged")
+	gamesList := flag.Bool("games", false, "list all the visible games")
+
+	flag.Parse()
+
 	cl, _ := api.NewClient()
 
 	if err := cl.LoginWithCredentials("ww", "a"); err != nil {
 		exitErr(err)
 	}
 
-	info, err := cl.APIInfo()
-
-	if err != nil {
-		exitErr(err)
+	if *apiMethods {
+		if info, err := cl.APIInfo(); err != nil {
+			exitErr(err)
+		} else {
+			var keys []string
+			for k := range info.Doc {
+				keys = append(keys, k)
+			}
+			fmt.Printf("API methods: %s\n", strings.Join(keys, ", "))
+		}
 	}
 
-	var keys []string
-	for k := range info.Doc {
-		keys = append(keys, k)
-	}
-	fmt.Printf("API methods: %s\n", strings.Join(keys, ", "))
-
-	s, err := cl.WhoAmI()
-
-	if err != nil {
-		exitErr(err)
+	if *whoAmI {
+		if s, err := cl.WhoAmI(); err != nil {
+			exitErr(err)
+		} else {
+			fmt.Printf("Username: %s\n", s)
+		}
 	}
 
-	fmt.Printf("Username: %s\n", s)
-
-	if err := cl.Logout(); err != nil {
-		exitErr(err)
+	if *gamesList {
+		if games, err := cl.ListGames(); err != nil {
+			exitErr(err)
+		} else {
+			fmt.Println("Available games:")
+			for _, g := range games {
+				fmt.Printf("- %s\n", g)
+			}
+		}
 	}
+
+	cl.Logout()
 }
