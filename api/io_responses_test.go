99f0502f6570a994e76cb785215d7da69c42cdfc
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-10 10:04:37 +0100

    100% code coverage on api/io_responses

--- api/io_responses_test.go
+++ api/io_responses_test.go
@@ -2,11 +2,36 @@ package api
 
 import (
 	"errors"
+	"fmt"
 	"github.com/franela/goblin"
 	"github.com/franela/goreq"
 	o "github.com/onsi/gomega"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 )
+
+func NewFakeHTTPSJSONServer() *httptest.Server {
+	return httptest.NewTLSServer(http.HandlerFunc(func(
+		w http.ResponseWriter, r *http.Request) {
+
+		method := r.Method
+		path := r.URL.Path
+
+		switch path {
+		case "/0/empty":
+			w.WriteHeader(200)
+
+		case "/0/empty-object":
+			w.WriteHeader(200)
+			fmt.Fprint(w, "{}")
+
+		case "/0/method":
+			w.WriteHeader(200)
+			fmt.Fprintf(w, `{"method": "%s"}`, method)
+		}
+	}))
+}
 
 func TestIOResponses(t *testing.T) {
 
@@ -35,10 +60,29 @@ func TestIOResponses(t *testing.T) {
 				o.Expect(sr.IsError()).To(o.BeFalse())
 				o.Expect(sr.Error()).To(o.BeNil())
 			})
+
+			g.It("Should return an error from the code in .Response", func() {
+				sr := simpleResponse{Status: "error"}
+				sr.Response.Error_code = 61760457
+				o.Expect(sr.IsError()).To(o.BeTrue())
+				o.Expect(sr.Error()).To(o.Equal(ErrWrongCmd))
+			})
 		})
 	})
 
 	g.Describe("Body", func() {
+
+		var ts *httptest.Server
+		var h *Httclient
+
+		g.Before(func() { ts = NewFakeHTTPSJSONServer() })
+		g.After(func() { ts.Close() })
+
+		g.BeforeEach(func() {
+			h = NewHTTClient()
+			h.baseURL = ts.URL
+		})
+
 		g.Describe(".IsEmpty()", func() {
 			g.It("Should return true if .Content is nil", func() {
 				b := Body{Content: nil}
@@ -74,6 +118,52 @@ func TestIOResponses(t *testing.T) {
 				b := Body{Content: &goreq.Body{}}
 				o.Expect(b.IsEmpty()).To(o.BeFalse())
 				o.Expect(b.Error()).To(o.BeNil())
+			})
+		})
+
+		g.Describe(".FromJSONTo(...)", func() {
+			g.It("Shouldn't return nil if .Error() is true", func() {
+				target := struct{ foo int }{}
+				b := Body{Content: nil, err: errDummy}
+				o.Expect(b.FromJSONTo(&target)).To(o.Equal(errDummy))
+			})
+
+			g.It("Should return ErrEmptyBody if the body is nil", func() {
+				target := struct{ foo int }{}
+				b := Body{Content: nil}
+				o.Expect(b.FromJSONTo(&target)).To(o.Equal(ErrEmptyBody))
+			})
+
+			g.It("Should return an error if the body is empty", func() {
+				b := h.call(get, "/empty", nil)
+				target := struct{ foo int }{}
+				o.Expect(b.FromJSONTo(&target)).NotTo(o.BeNil())
+			})
+
+			g.It("Should return nil if the body hasn't any field", func() {
+				b := h.call(get, "/empty-object", nil)
+				target := struct{ foo int }{foo: 42}
+				o.Expect(b.FromJSONTo(&target)).To(o.BeNil())
+				o.Expect(target.foo).To(o.Equal(42))
+			})
+
+			g.It("Should populate the target struct", func() {
+				b := h.call(get, "/method", nil)
+				target := struct{ Method string }{}
+				o.Expect(b.FromJSONTo(&target)).To(o.BeNil())
+				o.Expect(target.Method).To(o.Equal("GET"))
+			})
+		})
+
+		g.Describe(".Close()", func() {
+			g.It("Should return nil if the body is nil", func() {
+				b := Body{Content: nil}
+				o.Expect(b.Close()).To(o.BeNil())
+			})
+
+			g.It("Should return nil if the body was successfully closed", func() {
+				b := h.call(get, "/empty-object", nil)
+				o.Expect(b.Close()).To(o.BeNil())
 			})
 		})
 	})
