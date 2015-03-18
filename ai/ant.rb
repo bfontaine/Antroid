248f4fc80a2a6136329569d7851e55150905b2f9
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-18 22:17:59 +0100

    basic Ruby AI added

---
+++ ai/ant.rb
@@ -0,0 +1,71 @@
+#! /usr/bin/env ruby
+# -*- coding: UTF-8 -*-
+
+module AI
+  class << self
+
+    def next_line
+      $stdin.readline.chomp.split(" ").map(&:to_i)
+    end
+
+    def write(s)
+      $stdout.write s
+      $stdout.flush
+    end
+
+    def log(s)
+      s += "\n" unless s.end_with? "\n"
+      @_log.write s
+      @_log.flush
+    end
+
+    def read_message
+      log "reading:"
+
+      t, a, p, s = next_line
+      log " turn #{t}, #{a} ants per player (#{p} players), status=#{s}"
+
+      a.times do
+        id, x, y, _, _, e, a, b = next_line
+        log "  - ant ##{id} (#{x},#{y}) energy=#{e} acid=#{a} brain=#{b}"
+      end
+
+      n = next_line.first
+      log " #{n} other ants"
+
+      n.times do
+        x, y, _, _, b = next_line
+        log "  - ant in (#{x}, #{y}), brain=#{b}"
+      end
+
+      w, h, n = next_line
+      log " map #{w}x#{h}, #{n} points"
+
+      n.times do
+        x, y, c, s = next_line
+        log "  - (#{x}, #{y}) : #{c} (seen=#{s})"
+      end
+    end
+
+    def turn
+      read_message
+
+      log "go!"
+      write "0:forward\n"
+    rescue EOFError
+      log "that's the end."
+      @end = true
+    end
+
+    def run(log)
+      File.open(log, "a") do |f|
+        @_log = f
+        @end = false
+
+        turn until @end
+      end
+    end
+  end
+end
+
+AI.run "/tmp/rb-ai.log"
