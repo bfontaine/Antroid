b3a30ca947cd4ce1253c298fc24a0598002a9ea4
Julien Sagot <ju.sagot@gmail.com>
2015-03-19 09:12:38 +0100

    Added helper for reading a line.

--- ai/scout.scm
+++ ai/scout.scm
@@ -7,17 +7,28 @@
 ;; If it encounter a enemy ant, it will run away from it
 ;; If possible, this ant avoid going on a cell it knows
 
+;; Note: global variables names are UPPERCASE.
+
 ;; Libraries import
 (use readline loops matchable)
+
+;; Read a line on stdin, split using ' ' as delim
+(define (input-line)
+  (string-split (read-line) ) )
+
+;; Same as input-line, but also cast resulting list to int list
+(define (input-int-line)
+  (map (lambda (x) (string->number x))
+       (string-split (read-line) ) ) )
 
 ;; Read the first line of server input
 ;; [T, A, P and S] are global variables used be other reading functions
 (define (read-header)
-  (match-let (( (t a p s) (string-split (read-line) ) ))
-             (set! T (string->number t)) ;; Turn number
-             (set! A (string->number a)) ;; Ant / player
-             (set! P (string->number p)) ;; Number of players
-             (set! S (string->number s)) ;; Game status. 1: playing, 0: over
+  (match-let (( (t a p s) (input-int-line) ))
+             (set! T t) ;; Turn number
+             (set! A a) ;; Ant / player
+             (set! P p) ;; Number of players
+             (set! S s) ;; Game status. 1: playing, 0: over
              )
   )
 
