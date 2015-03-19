7d26263a06e427fd78fddcc2873d9d59b3682422
Julien Sagot <ju.sagot@gmail.com>
2015-03-19 08:39:06 +0100

    AI/scout: Added comments.

--- ai/scout.scm
+++ ai/scout.scm
@@ -1,7 +1,17 @@
+;; Be sure that readline, loops and matchable librairies are installed
+;; (e.g. with `[sudo] chicken-install readline loops matchable`)
+
+;; Run this AI with: csi scout.scm
+
+;; This AI is meant to explore the map
+;; If it encounter a enemy ant, it will run away from it
+;; If possible, this ant avoid going on a cell it knows
+
 ;; Libraries import
 (use readline loops matchable)
 
 ;; Read the first line of server input
+;; [T, A, P and S] are global variables used be other reading functions
 (define (read-header)
   (match-let (( (t a p s) (string-split (read-line) ) ))
              (set! T (string->number t)) ;; Turn number
