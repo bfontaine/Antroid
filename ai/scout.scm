04e549ee3a41f0810e72145adb06d95042c97ccc
Julien Sagot <ju.sagot@gmail.com>
2015-03-20 15:11:47 +0100

    AI/scout: Lot of fixes in associative list building. Basic AI draft.

--- ai/scout.scm
+++ ai/scout.scm
@@ -1,7 +1,7 @@
 #!/usr/bin/env csi
 
 ;; Be sure that loops and matchable librairies are installed
-;; (e.g. with `[sudo] chicken-install loops matchable`)
+;; (e.g. with `[sudo] chicken-install loops matchable list-utils`)
 
 ;; Run this AI with: csi scout.scm
 
@@ -12,7 +12,7 @@
 ;; Note: global variables names are UPPERCASE.
 
 ;; Libraries import
-(use loops matchable)
+(use loops matchable list-utils)
 
 ;; Read a line on stdin, split using ' ' as delim
 (define (input-line)
@@ -34,43 +34,98 @@
              )
   )
 
+;; We use it **a lot**
+(define li list)
+
+(define (<> a b) (not (= a b) ) )
+
 ;; Read [A] lines and fill the [ANTS] list reading next lines
 ;; [ANT] associate an ID to a list of parameters.
 (define (read-ants)
-  ( set! ANTS '() ) ;; reset ANTS
-  ( do-times _ A (match-let (( (id x y dx dy e a b) (input-int-line) ))
-                            ( set! ANTS (cons (list id (list x y dx dy e a b))
-                                              ANTS ) ) ) ) )
+  ( set! ANTS (li) ) ;; reset ANTS
+  ( do-times
+    _ A (match-let (( (id x y dx dy e a b) (input-int-line) ))
+                   (set! ANTS (append ANTS (li (li id (li x y dx dy e a b) ) )
+                                      ) ) ) ) )
 
 ;; Read the header line (the number of lines to parse)
 ;; Fill the [ENEMIES] list.
 (define (read-enemies)
   (let ((e (string->number (read-line) ) ))
     ( set! ENEMIES '() ) ;; reset ENEMIES
-    ( do-times _ e (set! ENEMIES (cons (input-int-line) ENEMIES) ) ) ) )
+    ( do-times _ e (set! ENEMIES (li (input-int-line) ENEMIES) ) ) ) )
 
 ;; Read the header line containing [W H N] and then
 ;; read the next lines according the header,
 ;; filling [MAP].
 (define (read-map)
   (match-let (( (w h n) (input-int-line) ))
-             ( set! MAP '() ) ;; reset MAP
-             ( do-times _ n (match-let (( (x y c s) (input-int-line) ))
-                                       ( set! MAP (cons (list (list x y)
-                                                              (list c s) )
-                                                        MAP ) ) ) ) ) )
+             ( set! MAP (li) ) ;; reset MAP
+             ( do-times
+               _ n (match-let (( (x y c s) (input-int-line) ))
+                              ( set! MAP (append (li (li (li x y)
+                                                         (li c s) ) )
+                                                 MAP ) ) ) ) ) )
+
+;; Helpers to test a cell content
+(define (rock? c) (= 2))
+(define (water? c) (= 4))
+
+;; Helpers to write an ant move
+(define (action a act) (string-append (number->string a) ":" act) )
+(define (rest a) (action a "rest") )
+(define (forward a) (action a "forward") )
+(define (right a) (action a "right") )
+(define (left a) (action a "left") )
+
+;; Return the cell fetched at [(x, y)]
+;; or [#f] if no cell is known here
+(define (cell x y) (assoc-def (li x y) MAP equal? #f) )
+
+(define (ant a) (cadr (assoc a ANTS equal?) ) )
+
+;; [#f] if a cell pointed by [x y] is already known
+;; list-utils is needed because primitive key comparison is not
+;; working with key which are pairs
+(define (unknown-cell? x y) (not (cell x y) ) )
+
+;; Test if the cell at [(x, y)] is safe or not
+;; FIXME: should also test if an ant is already on this cell or not
+(define (walkable-cell? x y)
+  (match-let (( (c _) (cell x y) )) (not (or (rock? c) (water? c) ) ) ) )
+
+;; For a given [ant] ant, choose the next move.
+;; FIXME: This function will keep an ant turning right / left
+;; without moving if it come to a point where it can not find any
+;; unknown cell close.
+(define (choose-move antid)
+  (match-let (( (x y dx dy e a b) (ant antid) ))
+             (cond
+
+              ;; brain is not controlled: send dummy [rest] instruction?
+              ((<> 1 b) (rest antid))
+
+              ;; two cells forward is an unkown cell and the forward cell
+              ;; can be walked on: go [forward]
+              ((and (unknown-cell? (+ (+ x dx) dx) (+ (+ y dy) dy) )
+                    (walkable-cell? (+ x dx) (+ y dy) ) )
+               (forward antid) )
+
+              ;; Randomly change the ant orientation if forward is not
+              ;; a suitable move
+              (else (if (= 0 (random 2)) (left antid) (right antid) ) )
+              ) ) )
 
 ;;;; Main loop
 
 (do-forever (read-header)
-            (if (= S 0) (exit) (print "turn:" T
-                                      " ant/player: " A
-                                      " players: " P
-                                      " status: " S))
-            (read-enemies)
-            (print "enemies:" ENEMIES)
+            (if (= S 0) (exit) '())
             (read-ants)
-            (print "ants:" ANTS)
+;;            (print "ants:" ANTS)
+            (read-enemies)
+;;            (print "enemies:" ENEMIES)
             (read-map)
-            (print "map:" MAP)
+;;            (print "map:" MAP)
+;;            (print "moves:")
+            (print (choose-move 0))
             )
