32a7f59f736514de73b17981fd486162e846462c
Baptiste Fontaine <batifon@yahoo.fr>
2015-03-21 18:36:51 +0100

    missing comments added.
    Fixes #8

--- api/server.go
+++ api/server.go
@@ -1,5 +1,7 @@
 package api
 
+// This file describes our local game server, implemented as a `Player` struct.
+
 import (
 	"bytes"
 	"fmt"
@@ -7,20 +9,40 @@ import (
 )
 
 // A Player represents a local game server connected to the remote one and
-// which sends and receive messages from it.
+// which sends and receive messages from it. It used an API client, an AI pool
+// and a Listeners pool to communicate with the remote server, send turn infos
+// to the AIs and get their commands back, and send turn infos to the plugins.
+// One of these plugins can be the GUI, for example.
+//
+// See docs/ai_protocol.md for the protocol we use to communicate with AIs and
+// Plugin Listeners.
+//
+// A player represents one player in one game. At each turn, it'll send a
+// command to play followed by a call to get the game's status. The first time,
+// it'll send a dummy play command where all ants are resting just to get their
+// positions, since we don't have ants' positions when creating/joining a game.
 type Player struct {
-	Client    *Client
-	AIs       *AIPool
+	// API client
+	Client *Client
+	// All AIs
+	AIs *AIPool
+	// All plugin Listeners
 	Listeners *ListenersPool
 
+	// User credentials
 	username, password string
 
+	// If this is true we'll print more info
 	debug bool
 
-	status     *GameStatus
-	turn       *Turn
+	// The current game status
+	status *GameStatus
+	// The current turn
+	turn *Turn
+	// The map as we know it. This is updated at each turn
 	partialMap *PartialMap
 
+	// This will be true when the game will end
 	done bool
 }
 
@@ -77,6 +99,7 @@ func (p *Player) JoinGame(id GameID) (err error) {
 		return
 	}
 
+	// we request the game's status to have all its parameters
 	if p.status, err = p.Client.GetGameIdentifierStatus(id); err != nil {
 		return
 	}
@@ -95,12 +118,15 @@ func (p *Player) JoinGame(id GameID) (err error) {
 	}
 
 	commands := Commands(restCmd.String())
+
+	// "play" with this rest command
 	p.turn, err = p.Client.PlayIdentifier(p.status.Identifier, commands)
 
 	if err != nil {
 		return
 	}
 
+	// start all plugins, including AIs
 	p.startPlugins()
 
 	return
@@ -121,7 +147,8 @@ func (p *Player) PlayTurn() (done bool, err error) {
 	return
 }
 
-// Quit stops all AIs and logout the player from the remote server
+// Quit stops all AIs and Listeners and logout the player from the remote
+// server
 func (p *Player) Quit() error {
 	p.AIs.Stop()
 	p.Listeners.Stop()
@@ -132,6 +159,7 @@ func (p *Player) Quit() error {
 func (p *Player) PrintScores() {
 	var usernameMaxSize int
 
+	// get the longest username
 	for username := range p.status.Score {
 		usernameSize := len(username)
 
@@ -140,23 +168,30 @@ func (p *Player) PrintScores() {
 		}
 	}
 
+	// craft the output format to be large enough to contain the longest
+	// username
 	format := fmt.Sprintf("%%-%ds: %%d\n", usernameMaxSize)
 
+	// print each score
 	for user, score := range p.status.Score {
 		fmt.Printf(format, user, score)
 	}
 }
 
+// updateStatus calls the remote server for the game status and updates the
+// player's one.
 func (p *Player) updateStatus() (err error) {
 	p.status, err = p.Client.GetGameIdentifierStatus(p.status.Identifier)
 	return
 }
 
+// startPlugins starts all AIs and all Listeners
 func (p *Player) startPlugins() {
 	p.AIs.Start()
 	p.Listeners.Start()
 }
 
+// internal helper to get a number for an ant's brain state
 func brainNumber(a BasicAntStatus) int {
 	if a.Brain == "controlled" {
 		return 1
@@ -166,6 +201,7 @@ func brainNumber(a BasicAntStatus) int {
 	return 0
 }
 
+// internal helper to get a number for a cell's visibility
 func visibilityNumber(c Cell) int {
 	if c.Visibility {
 		return 1
@@ -174,7 +210,7 @@ func visibilityNumber(c Cell) int {
 	return 0
 }
 
-// see the format spec
+// cell contents, see `docs/ai_protocol.md`
 var contents = map[string]int{
 	"grass": 0,
 	"rock":  2,
@@ -184,6 +220,7 @@ var contents = map[string]int{
 	"meat":  5,
 }
 
+// internal helper to get a number for a cell's content
 func contentNumber(c Cell) (v int) {
 	v, ok := contents[c.Content]
 	if !ok {
@@ -193,6 +230,8 @@ func contentNumber(c Cell) (v int) {
 	return
 }
 
+// constructs a message describing the current turn (see `docs/ai_protocol.md`)
+// and send it to all plugins and AIs.
 func (p *Player) sendTurnStatusToPlugins() {
 	var buf bytes.Buffer
 
@@ -202,6 +241,7 @@ func (p *Player) sendTurnStatusToPlugins() {
 		playing = 0
 	}
 
+	// header
 	buf.WriteString(fmt.Sprintf("%d %d %d %d\n",
 		p.turn.Number,                    // T
 		p.status.Game.Spec.AntsPerPlayer, // A
@@ -211,6 +251,7 @@ func (p *Player) sendTurnStatusToPlugins() {
 
 	var visibleAnts, enemyAnts []BasicAntStatus
 
+	// all our ants
 	for _, ant := range p.turn.AntsStatuses {
 		// save visible ants
 		for _, visible := range ant.VisibleAnts {
@@ -244,9 +285,10 @@ func (p *Player) sendTurnStatusToPlugins() {
 		enemyAnts = append(enemyAnts, visible)
 	}
 
-	// N
+	// N enemy ants
 	buf.WriteString(fmt.Sprintf("%d\n", len(enemyAnts)))
 
+	// enemy ants
 	for _, ant := range enemyAnts {
 		buf.WriteString(fmt.Sprintf("%d %d %d %d %d\n",
 			ant.Pos.X,        // X
@@ -257,12 +299,14 @@ func (p *Player) sendTurnStatusToPlugins() {
 		))
 	}
 
+	// map header
 	buf.WriteString(fmt.Sprintf("%d %d %d\n",
 		p.partialMap.Width(),    // W
 		p.partialMap.Height(),   // H
 		len(p.partialMap.Cells), // N
 	))
 
+	// map cells
 	for _, cell := range p.partialMap.Cells {
 		buf.WriteString(fmt.Sprintf("%d %d %d %d\n",
 			cell.Pos.X,              // X
@@ -275,13 +319,17 @@ func (p *Player) sendTurnStatusToPlugins() {
 	msg := buf.String()
 
 	if p.debug {
+		// print the message if we're debugging
 		fmt.Fprintf(os.Stderr, "%s\n", msg)
 	}
 
+	// send it
 	p.AIs.SendAll(msg)
 	p.Listeners.SendAll(msg)
 }
 
+// playTurn gets the command to use from all AIs, send it to the server and
+// updates the local game status.
 func (p *Player) playTurn() (err error) {
 	cmd := p.AIs.GetCommandResponse()
 
